ed96f0b21663717dc8a4e0bbf171ebd5b66ceeb5
agustin kainer <puchidev98@gmail.com>
2025-04-11 21:11:50 -0300

    agregando router admin

--- back/src/routes/index.router.js
+++ back/src/routes/index.router.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const routerAlumn = require("./usuarios/allAlumn");
 const routerProfesor = require("./usuarios/profesor");
+const routerAdmin = require("./usuarios/admin");
 const router = Router();
 
 router.use("/alumnos", routerAlumn);
 // PROFESORES
 router.use("/profesores", routerProfesor);
+//ADMIN
+router.use("/admin", routerAdmin);
+
 module.exports = router;
